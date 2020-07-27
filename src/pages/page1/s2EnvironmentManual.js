6255555664e71b91af1507c23a1be249eb6be471
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-07-27 15:24:45 +0900

    page1の内容編集

--- src/pages/page1/s2EnvironmentManual.js
+++ src/pages/page1/s2EnvironmentManual.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import SEO from './../../parts/SEO'
+import codes from './../../config/codes'
+import Parts from './../../config/parts'
 
 const EnvironmentManual = ({allData}) => {
 	return (
@@ -8,7 +10,7 @@ const EnvironmentManual = ({allData}) => {
 			<div className="page-layout">
 
 				<h2 className="h2">
-					1. ディレクトリ・ファイル 作成
+					1. ディレクトリ作成
 				</h2>
 					<p className="p">
 						プロジェクト用のディレクトリを作成し、以下のコマンドを実行し、package.jsonを作成
@@ -19,131 +21,51 @@ const EnvironmentManual = ({allData}) => {
 						</span>
 					</p>
 					<p className="p">
-						以下のコマンドで必要なファイルも作成しておく
+						作成したプロジェクトのディレクトリに移動し、以下のコマンドで必要なフォルダを作成
 					</p>
 					<p className="p">
 						<span className="command">
-							$ touch index.html<br/>
-							$ touch app.js
+							$ mkdir pages(src/pagesでも可)
 						</span>
 					</p>
 					<p className="p">
-						index.htmlはbodyの中に以下のタグを記載（このタグの中にレンンダリングされていく）
-					</p>
-
-					<p className="p">
-						app.jsは以下のコードを記入
+						このpages(src/pages)フォルダは、ページコンポーネントを格納し、ルーティングをするために必須
 					</p>
 
 				<h2 className="h2">
-					2. reactとreact-domのインストール
+					2. nextとreactとreact-domのインストール
 				</h2>
 					<p className="p">
-						Reactを使えるようにする
+						NextとReactを使えるようにする
 					</p>
 					<p className="p">
-						以下のコマンドを実行するとプロジェクト配下にnode_modulesフォルダが作成され、package.jsonのdependenciesにreactとreact-domが記載される
+						以下のコマンドを実行するとプロジェクト配下にnode_modulesフォルダが作成され、package.jsonのdependenciesにnextとreactとreact-domが記載される
 					</p>
 					<p className="p">
 						<span className="command">
-						$ npm install --save react react-dom
+							$ npm install --save react react-dom next
 						</span>
 					</p>
 
 				<h2 className="h2">
-					3. Webpackのインストール
+					3. npm-scriptsの設定
 				</h2>
 					<p className="p">
 						ファイルのビルド関連の設定をする
 					</p>
 					<p className="p">
-						以下のコマンドでグローバルにインストール（上手く動作しない場合があるため、グローバルにインストール）
+						以下のようにpackage.jsonに追加する
+					</p>
+					<Parts.CodeHighlighter
+						language={codes.languages.jsx}
+						codeString={codes.pages.Page1.Section2.code1}
+					/>
+					<p className="p">
+						以下のコマンドを実行することでnextの開発環境が立ち上がり、404のエラー画面が表示されればOK（まだindex.jsを作成していないため）
 					</p>
 					<p className="p">
 						<span className="command">
-							$ npm install -g webpack-cli
-						</span>
-					</p>
-					<p className="p">
-						以下のコマンドで、インストール＆package.jsonに記載
-					</p>
-					<p className="p">
-						<span className="command">
-							$ npm install --save-dev webpack<br/>
-							$ npm install --save-dev webpack-cli
-						</span>
-					</p>
-					<p className="p">
-						以下のコマンドでWebpackの設定ファイルを作成
-					</p>
-					<p className="p">
-						<span className="command">
-							$ touch webpack.config.js
-						</span>
-					</p>
-					<p className="p">
-						作成されたファイルに以下の内容を追加
-					</p>
-
-					<p className="p">
-						index.htmlに以下のコードを追加
-					</p>
-
-				<h2 className="h2">
-					4. Babelのインストール
-				</h2>
-					<p className="p">
-						JSX(xml風のJavascript)で書かれたものをJSに変換する
-					</p>
-					<p className="p">
-						以下のコマンドを実行でBabelをインストール＆package.jsonに記載
-					</p>
-					<p className="p">
-						<span className="command">
-							$ npm install --save-dev babel-loader babel-core babel-preset-es2015 babel-preset-react
-						</span>
-					</p>
-					<p className="p">
-						以下のコマンドで.babelrcファイルを作成し、できたファイルに以下のコードを追加
-					</p>
-					<p className="p">
-						<span className="command">
-							$ touch .babelrc
-						</span>
-					</p>
-
-
-				<h2 className="h2">
-					5. ブラウザのリロード実装
-				</h2>
-					<p className="p">
-						毎回コマンドを実行せずに、コードを更新する度にブラウザのオートリロードをする設定
-					</p>
-					<p className="p">
-						package.jsonに以下のコードを追加
-					</p>
-
-
-				<h2 className="h2">
-					6. サーバ起動
-				</h2>
-					<p className="p">
-						以下を実行し、「http://localhost:8080 」にアクセスで画面が表示(app.jsにReact記述した内容が表示)
-					</p>
-					<p className="p">
-						・webpackの実行
-					</p>
-					<p className="p">
-						<span className="command">
-							$ npm run build
-						</span>
-					</p>
-					<p className="p">
-						・自動リロード
-					</p>
-					<p className="p">
-						<span className="command">
-							$ npm start
+							$ npm run dev
 						</span>
 					</p>
 
--- src/pages/page1/s3AboutEnv.js
+++ src/pages/page1/s3AboutEnv.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import SEO from './../../parts/SEO'
+import urls from './../../config/outerUrls'
+import codes from './../../config/codes'
+import Parts from './../../config/parts'
 
 const AboutEnv = ({allData}) => {
 	return (
@@ -14,7 +17,7 @@ const AboutEnv = ({allData}) => {
 						環境に合わせてルートディレクトリに以下のようなファイル作成する
 					</p>
 					<p className="p">
-						.env.development・・・ローカル開発環境<br/>
+						.env.development・・・開発環境<br/>
 						.env.production・・・本番環境
 					</p>
 					<p className="p">
@@ -25,11 +28,15 @@ const AboutEnv = ({allData}) => {
 					2. KEYの設定ルール
 				</h2>
 					<p className="p">
-						作成したファイルに記載するKEYは必ず「REACT_APP_」を頭につけること
+						作成したファイルに記載するKEYは必ず「NEXT_PUBLIC_」を頭につけること
 					</p>
 					<div className="p">
 						Ex.)
 					</div>
+					<Parts.CodeHighlighter
+						language={codes.languages.javascript}
+						codeString={codes.pages.Page1.Section3.code1}
+					/>
 
 
 				<h2 className="h2">
@@ -38,13 +45,38 @@ const AboutEnv = ({allData}) => {
 					<p className="p">
 						以下のような記述で呼び出せる
 					</p>
-
-					<div className="p">
-						Ex.)
-					</div>
+					<Parts.CodeHighlighter
+						language={codes.languages.javascript}
+						codeString={codes.pages.Page1.Section3.code2}
+					/>
 
 					<p className="p">
-						上記で「React Learning」を取得できる
+						上記の設定を利用すると、以下で「Next Learning」を取得できる
+					</p>
+					<Parts.CodeHighlighter
+						language={codes.languages.javascript}
+						codeString={codes.pages.Page1.Section3.code3}
+					/>
+
+				<h2 className="h2">
+					※ next.config.jsを利用した場合
+				</h2>
+					<p className="p">
+						next.config.jsで以下のようにエクスポートすることでも環境変数の設定ができる
+					</p>
+					<Parts.CodeHighlighter
+						language={codes.languages.javascript}
+						codeString={codes.pages.Page1.Section3.code4}
+					/>
+
+					<p className="p">
+						使用する側では通常通りと同様に指定したKey名(上記の場合、customKey)で取得できる
+					</p>
+					<p className="p">
+						<Parts.OuterLink
+							url={urls.NextOfficial.EnvironmentVariables}
+							linkText="? next.config.jsでの環境変数の設定"
+						/>
 					</p>
 
 			</div>
